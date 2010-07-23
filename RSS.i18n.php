9dac5e8b6677d0f85386899757444666467291c1
Raimond Spekking <raymond@users.mediawiki.org>
2010-07-23 21:34:35 +0000

    Localisation updates for core and extension messages from translatewiki.net (2010-07-23 21:25:00 UTC)

--- RSS.i18n.php
+++ RSS.i18n.php
@@ -17,6 +17,15 @@ $messages['en'] = array(
 	'rss-empty' => 'Failed to load RSS feed from $1!',
 );
 
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'rss-desc' => 'Ermöglicht es, einen RSS-Feed auf einer Wikiseite anzuzeigen',
+	'rss-error' => 'Der RSS-Feed konnte nicht von $1 geladen werden: $2',
+	'rss-empty' => 'Der RSS-Feed konnte nicht von $1 geladen werden!',
+);
+
 /** Finnish (Suomi)
  * @author Jack Phoenix <jack@countervandalism.net>
  */
@@ -25,18 +34,53 @@ $messages['fi'] = array(
 	'rss-empty' => 'RSS-syötteen lataaminen osoitteesta $1 epäonnistui!',
 );
 
+/** Interlingua (Interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'rss-desc' => 'Monstra un syndication RSS in un pagina wiki',
+	'rss-error' => 'Falleva de cargar le syndication RSS de $1: $2',
+	'rss-empty' => 'Falleva de cargar le syndication RSS de $1!',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'rss-desc' => 'Прикажува RSS-канал на викистраница',
+	'rss-error' => 'Не успеав да го вчитам RSS-каналот од $1: $2',
+	'rss-empty' => 'Не успеав да го вчитам RSS-каналот од $1!',
+);
+
 /** Dutch (Nederlands)
  * @author Mitchel Corstjens
+ * @author Siebrand
  */
 $messages['nl'] = array(
+	'rss-desc' => 'Biedt een RSS-feed voor een wikipagina',
 	'rss-error' => 'Kon RSS feed van $1 niet laden, fout: $2',
 	'rss-empty' => 'Kon RSS feed van $1 niet laden!',
 );
 
-/** Polish (Polski)
- * @author Łukasz Garczewski (TOR) <tor@wikia-inc.com>
- */
+/** Polish (Polski) */
 $messages['pl'] = array(
 	'rss-error' => 'Nie udało się odczytać kanału $1: $2',
 	'rss-empty' => 'Nie udało się odczytać kanału $1!',
 );
+
+/** Portuguese (Português)
+ * @author Giro720
+ */
+$messages['pt'] = array(
+	'rss-desc' => 'Mostra um feed RSS em uma página wiki',
+);
+
+/** Brazilian Portuguese (Português do Brasil)
+ * @author Giro720
+ */
+$messages['pt-br'] = array(
+	'rss-desc' => 'Mostra um feed RSS em uma página wiki',
+	'rss-error' => 'Falha ao carregar feed RSS de $1: $2',
+	'rss-empty' => 'Falha ao carregar feed RSS de $1!',
+);
+
